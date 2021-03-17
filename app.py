b5abd57e511df67222afed2b98bb42cc46ccb3fa
CKChang <benctw@gmail.com>
2021-03-17 15:04:41 +0800

    cookie

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, url_for, request, render_template, request, redirect
+from flask import Flask, url_for, request, render_template, request, redirect, make_response
 from markupsafe import escape
+import time
 
 def valid_login(email, password):
     if (email=='benctw@gmail.com' and password=='kk123'):
@@ -26,12 +27,18 @@ def login():
     if request.method == 'POST':
         if valid_login(request.form['email'],
                        request.form['password']):
-            return redirect(url_for('hello', name=request.form['email']))
+            resp = make_response(redirect(url_for('hello', name=request.form['email'])))
+            resp.set_cookie('user_email', request.form['email'], expires=time.time()+60*60*24*7)
+            return resp
         else:
             error = '帳號/密碼錯誤'
             return error
     else:
-        return render_template('login.html')
+        user_email = request.cookies.get('user_email')
+        if user_email:
+            return redirect(url_for('hello', name=user_email))
+        else:
+            return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
