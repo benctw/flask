1d925c9fc76340d77db8ffafd42c72ca31a78913
CKChang <benctw@gmail.com>
2021-03-17 14:23:13 +0800

    check login

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, request, redirect
 from markupsafe import escape
 
-def do_the_login():
-    return "do_the_login"
-
-def show_the_login_form():
-    return render_template('login.html')
+def valid_login(email, password):
+    if (email=='benctw@gmail.com' and password=='kk123'):
+        return True
+    else:
+        return False
 
 app = Flask(__name__)
 
@@ -20,13 +20,16 @@ def hello(name=None):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        return do_the_login()
+        if valid_login(request.form['email'],
+                       request.form['password']):
+            return redirect(url_for('hello', name=request.form['email']))
+        else:
+            error = '帳號/密碼錯誤'
+            return error
     else:
-        return show_the_login_form()
-
-with app.test_request_context():
-    print(url_for('static', filename='style.css'))
+        return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
