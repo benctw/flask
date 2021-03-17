d53d8b3d44b05d7bbfa028ce8ccf547e13b511c2
CKChang <benctw@gmail.com>
2021-03-17 11:57:52 +0800

    url building

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return 'Homepage'
+def index():
+    return 'index'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/login')
+def login():
+    return 'login'
 
 @app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
