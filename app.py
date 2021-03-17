cc7f9f4049e4a2e99cb94389204217aa2a911d21
CKChang <benctw@gmail.com>
2021-03-17 11:34:30 +0800

    hello

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return 'Homepage'
-    
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
