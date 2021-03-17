cb925f894f54f348e5191ae4d5a5cf12be9b83e6
CKChang <benctw@gmail.com>
2021-03-17 12:06:44 +0800

    HTTP Methods

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 from markupsafe import escape
+
+def do_the_login():
+    return "do_the_login"
+
+def show_the_login_form():
+    return "show_the_login_form"
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'index'
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'login'
-
-@app.route('/user/<username>')
-def profile(username):
-    return '{}\'s profile'.format(escape(username))
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
