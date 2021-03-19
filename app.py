356c905acc7cdaba6c3e0e694944a3bb09faa68a
CKChang <benctw@gmail.com>
2021-03-19 15:47:27 +0800

    error page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
 from markupsafe import escape
 
 def do_the_login(email, password):
     if email=='benctw@gmail.com' and password=='kk123':
-        return "登入成功"
+        return redirect(url_for('hello', useremail=email))
     else:
-        return "登入失敗"
+        return redirect(url_for('error', errorcode=6000))
 
 app = Flask(__name__)
 
@@ -23,6 +23,16 @@ def login():
         return do_the_login(email, password)
     else:
         return render_template('login.html')
+
+
+@app.route('/hello')
+def hello():
+    useremail = request.args.get('useremail', '')
+    return f'Hello, {useremail}'
+
+@app.route('/error/<int:errorcode>')
+def error(errorcode):
+    return render_template('error.html', errorcode=errorcode)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
