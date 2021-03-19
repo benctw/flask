33a55da6dc442677bc5fbdfc8494e8acdb0d1db1
CKChang <benctw@gmail.com>
2021-03-19 14:27:59 +0800

    modify

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape
 
 def do_the_login():
     return "do_the_login"
 
-def show_the_login_form():
-    return "show_the_login_form"
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         return do_the_login()
     else:
-        return show_the_login_form()
+        return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
